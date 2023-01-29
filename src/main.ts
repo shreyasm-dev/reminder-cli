58318d7af1c0bb46ebf2bd50ae1cd14d9df9b475
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-29 10:38:48 -0600

    Set up commander

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
 import sourceMapSupport from 'source-map-support';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { program } from 'commander';
 
 sourceMapSupport.install();
+
+const pkg = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf8'));
+
+program
+  .name(pkg.name)
+  .version(pkg.version)
+  .description(pkg.description);
+
+program.parse(process.argv);
